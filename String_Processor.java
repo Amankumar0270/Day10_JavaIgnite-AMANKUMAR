1c2e1e3b145d2c044a8754af6e01bc9608ba1a81
rite2harini <rite2harini@gmail.com>
2026-06-05 14:11:18 +0530

    Create String_Processor.java

---
+++ String_Processor.java
@@ -0,0 +1,19 @@
+/*
+String Processor with Exception Safety
+
+Create a program that:
+
+Takes a string input
+
+Performs:
+
+substring operation
+charAt operation
+Requirements:
+
+Handle:
+
+StringIndexOutOfBoundsException
+Example:
+Invalid index handled safely
+*/
