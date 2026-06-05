3058d5985bc5e6b0150b26ca3e3ba7159e2cac05
rite2harini <rite2harini@gmail.com>
2026-06-05 14:11:58 +0530

    Create Banking_System.java

---
+++ Banking_System.java
@@ -0,0 +1,13 @@
+/*
+Banking System (Real-world Simulation)
+
+Create a program that:
+
+Takes account balance
+Takes withdrawal amount
+Rules:
+If withdrawal > balance → throw and handle exception
+If invalid input → handle exception
+Output:
+Withdrawal successful OR Insufficient balance
+*/
