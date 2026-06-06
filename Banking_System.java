788ff0a8e6bbc61d2bd14fd28ced7ad01615245f
Amankumar0270 <cse.25bcsi39@silicon.ac.in>
2026-06-06 23:38:06 +0530

    Update Banking_System.java

--- Banking_System.java
+++ Banking_System.java
@@ -11,3 +11,51 @@ If invalid input → handle exception
 Output:
 Withdrawal successful OR Insufficient balance
 */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class InsufficientBalanceException extends Exception {
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+public class BankingSystem {
+
+    public static void withdraw(double balance, double amount)
+            throws InsufficientBalanceException {
+
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance");
+        }
+
+        System.out.println("Withdrawal successful");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Enter Account Balance: ");
+            double balance = sc.nextDouble();
+
+            System.out.print("Enter Withdrawal Amount: ");
+            double amount = sc.nextDouble();
+
+            withdraw(balance, amount);
+
+        } 
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        } 
+        catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
