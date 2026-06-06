40674f40e842d8fb1a157cb69e2c9ab0401b7efb
Amankumar0270 <cse.25bcsi39@silicon.ac.in>
2026-06-06 23:39:37 +0530

    Update Student_Marks.java

--- Student_Marks.java
+++ Student_Marks.java
@@ -10,3 +10,32 @@ Add Exception Handling:
 Handle invalid index
 Handle invalid input (if user enters text instead of number)
 */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class StudentMarksSystem {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int marks[] = {78, 85, 90, 67, 88};
+
+        try {
+
+            System.out.print("Enter index (0 to 4): ");
+            int index = sc.nextInt();
+
+            System.out.println("Student Mark: " + marks[index]);
+
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Index");
+        } 
+        catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
+        }
+
+        sc.close();
+    }
+}
